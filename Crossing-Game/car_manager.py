f718ddca424b200dcd235c521040b5207005f4d8
batuhan-zeytun <batuhanzeytun2@gmail.com>
2025-04-22 21:56:40 +0300

    Update car_manager.py

--- Crossing-Game/car_manager.py
+++ Crossing-Game/car_manager.py
@@ -1,41 +1,41 @@
-# car_manager.py
 
-import random
 from turtle import Turtle
+import random
+
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 2
-MAX_CARS = 50  
+
 
 class CarManager:
-    def __init__(self, spawn_chance=0.1):
+    def __init__(self):
         self.cars = []
-        self.car_speed = STARTING_MOVE_DISTANCE
-        self.spawn_chance = spawn_chance
 
-    def _create_single_car(self):
-        new_car = Turtle("square")
-        new_car.shapesize(stretch_wid=1, stretch_len=2)
-        new_car.color(random.choice(COLORS))
-        new_car.penup()
-        new_car.goto(300, random.randint(-240, 240))
-        self.cars.append(new_car)
+    
+    def create_car(self):
+        random_chance = random.randint(1,6)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.color(random.choice(COLORS))
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.penup()
+            new_car.goto(300, random.randint(-250, 250))
+            self.cars.append(new_car)
 
     def move_cars(self):
-        if random.random() < self.spawn_chance and len(self.cars) < MAX_CARS:
-            self._create_single_car()
-
         for car in self.cars:
-            car.setx(car.xcor() - self.car_speed)
-
-        self.cars = [car for car in self.cars if car.xcor() > -320]
+            car.setx(car.xcor() - STARTING_MOVE_DISTANCE)
+        
+    def remove_cars(self):
+        for car in self.cars:
+            car.hideturtle()
+        self.cars = []
 
     def increase_speed(self):
         self.car_speed += MOVE_INCREMENT
 
     def reset(self):
+        self.remove_cars()
         self.car_speed = STARTING_MOVE_DISTANCE
-        for car in self.cars:
-            car.hideturtle()
-        self.cars.clear()
+        self.create_car()
